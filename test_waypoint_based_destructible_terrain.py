bd26ffad41869ad275f6b6ffd6c12e03543bdadc
A. Schäfer <arminnessie@gmail.com>
2024-02-11 16:28:37 +0100

    Add more tests.

--- test_waypoint_based_destructible_terrain.py
+++ test_waypoint_based_destructible_terrain.py
@@ -7,11 +7,17 @@ class Test(unittest.TestCase):
         graph = {}
         self.assertEqual(weightless_breadth_first_search(graph, 0, 0), [])
 
-        graph = {"0": [1], "1": []}
-        self.assertEqual(weightless_breadth_first_search(graph, 0, 1), [0, 1])
+        graph = {"0": [1], "1": [2], "2": []}
+        self.assertEqual(weightless_breadth_first_search(graph, 0, 2), [0, 1, 2])
+        self.assertEqual(weightless_breadth_first_search(graph, 2, 0), [])
 
-        graph = {"0": [1], "1": []}
-        self.assertEqual(weightless_breadth_first_search(graph, 1, 0), [])
+    def test_humanplayer(self):
+        human = HumanPlayer(500, 300, (255, 100, 0))
+        self.assertEqual(human.color, (255, 100, 0))
+
+    def test_aiplayer(self):
+        ai = AIPlayer(700, 500, (255, 0, 0))
+        self.assertEqual(ai.color, (255, 0, 0))
 
 
 if __name__ == "__main__":
