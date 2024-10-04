10dd0c7f0da84664f80a62c12d607c67263e7818
772 <772>
2024-10-04 16:43:58 +0200

    Update.

---
+++ rusty-clonk/README.md
@@ -0,0 +1,22 @@
+# Rusty Clonk
+
+This software is a Rust implementation of https://github.com/772/waypoint_based_destructible_terrain.
+
+This project is in a very early stage.
+
+## License
+
+Licensed under either of these:
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+   https://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or
+   https://opensource.org/licenses/MIT)
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+
+## Trademark
+
+'Clonk' is a registered trademark of Matthes Bender.
---
+++ rusty-clonk/src/main.rs
@@ -0,0 +1,272 @@
+#![allow(
+    clippy::type_complexity,
+    clippy::too_many_arguments,
+    clippy::collapsible_if
+)]
+use bevy::sprite::{Wireframe2dConfig, Wireframe2dPlugin};
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+#[derive(Debug, Resource)]
+struct Floors(Vec<Floor>);
+#[derive(Component, Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+#[derive(Component)]
+struct IsHumanoid;
+#[derive(Component)]
+struct ControledByPlayer;
+#[derive(Component, Debug, PartialEq)]
+enum Action {
+    Walking,
+    Falling,
+    Digging,
+}
+#[derive(Component)]
+struct CurrentFloor(Option<Floor>);
+
+const HGT_TUNNEL: f32 = 60.0;
+const HGT_HUMANOID: f32 = 40.0;
+const SPEED_WALKING: f32 = 3.0;
+const SPEED_DIGGING: f32 = 2.0;
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins((DefaultPlugins, Wireframe2dPlugin))
+        .add_systems(Startup, setup);
+    app.add_systems(Update, keyboard_control);
+    app.insert_resource(Floors(Vec::new()));
+    app.run();
+}
+
+#[derive(Clone, Debug)]
+struct Floor {
+    x_left: f32,
+    y_left: f32,
+    hgt_left: f32,
+    x_right: f32,
+    y_right: f32,
+    hgt_right: f32,
+    id_left_neighbor: Option<usize>,
+    id_right_neighbor: Option<usize>,
+}
+
+impl Floor {
+    pub fn new(x_left: f32, y_left: f32, x_right: f32, y_right: f32) -> Floor {
+        Floor {
+            x_left,
+            y_left,
+            hgt_left: HGT_TUNNEL,
+            x_right,
+            y_right,
+            hgt_right: HGT_TUNNEL,
+            id_left_neighbor: None,
+            id_right_neighbor: None,
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut all_floors: ResMut<Floors>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Camera.
+    commands.spawn(Camera2dBundle::default());
+
+    // Background.
+    let texture_earth = asset_server.load("texture_earth.png");
+    commands.spawn(SpriteBundle {
+        texture: texture_earth.clone(),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        global_transform: GlobalTransform::from_xyz(0.0, 0.0, 0.0),
+        ..default()
+    });
+
+    // Tunnel.
+    let mut floors: Vec<Floor> = vec![
+        Floor::new(-960.0, 100.0, -500.0, 100.0), // 0
+        Floor::new(-500.0, 100.0, -100.0, 0.0),   // 1
+        Floor::new(-100.0, 0.0, 0.0, 0.0),        // 2
+        Floor::new(0.0, 0.0, 300.0, 100.0),       // 3
+        Floor::new(300.0, 100.0, 800.0, -200.0),  // 4
+        Floor::new(800.0, -200.0, 960.0, -200.0),  // 5
+    ];
+    floors[0].id_right_neighbor = Some(1);
+    floors[1].id_left_neighbor = Some(0);
+    floors[1].id_right_neighbor = Some(2);
+    floors[2].id_left_neighbor = Some(1);
+    floors[2].id_right_neighbor = Some(3);
+    floors[3].id_left_neighbor = Some(2);
+    floors[3].id_right_neighbor = Some(4);
+    floors[4].id_left_neighbor = Some(3);
+    floors[4].id_right_neighbor = Some(5);
+    floors[5].id_left_neighbor = Some(4);
+    *all_floors = Floors(floors.clone());
+    let mut shapes: Vec<Mesh2dHandle> = Vec::new();
+    for floor in &floors {
+        shapes.push(Mesh2dHandle(meshes.add(Triangle2d::new(
+            Vec2::new(floor.x_left, floor.y_left),
+            Vec2::new(floor.x_left, floor.y_left + floor.hgt_left),
+            Vec2::new(floor.x_right, floor.y_right),
+        ))));
+        shapes.push(Mesh2dHandle(meshes.add(Triangle2d::new(
+            Vec2::new(floor.x_left, floor.y_left + floor.hgt_left),
+            Vec2::new(floor.x_right, floor.y_right + floor.hgt_right),
+            Vec2::new(floor.x_right, floor.y_right),
+        ))));
+    }
+    for (i, shape) in shapes.into_iter().enumerate() {
+        let color = Color::srgba(0.0, 0.0, 0.0, 0.7);
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: shape,
+            material: materials.add(color),
+            transform: Transform::from_xyz(0.0, 0.0, 0.1 + 0.001 * (i as f32)),
+            global_transform: GlobalTransform::from_xyz(0.0, 0.0, i as f32),
+            ..default()
+        });
+    }
+
+    // HUD.
+    commands.spawn(
+        TextBundle::from_section(
+            "Press space to toggle wireframes. Left / right arrow key to move player.",
+            TextStyle::default(),
+        )
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(12.0),
+            left: Val::Px(12.0),
+            ..default()
+        }),
+    );
+
+    // Humanoids.
+    let texture_player = asset_server.load("player.png");
+    commands.spawn((
+        SpriteBundle {
+            texture: texture_player.clone(),
+            transform: Transform::from_xyz(-50.0, 0.0 + HGT_HUMANOID / 2.0, 1.0),
+            ..default()
+        },
+        IsHumanoid,
+        Action::Walking,
+        Direction::Left,
+        CurrentFloor(Some(floors[2].clone())),
+    ));
+    commands.spawn((
+        SpriteBundle {
+            texture: texture_player.clone(),
+            transform: Transform::from_xyz(0.0, 0.0 + HGT_HUMANOID / 2.0, 1.0),
+            ..default()
+        },
+        IsHumanoid,
+        ControledByPlayer,
+        Action::Walking,
+        Direction::Left,
+        CurrentFloor(Some(floors[2].clone())),
+    ));
+}
+
+fn keyboard_control(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    all_floors: ResMut<Floors>,
+    mut humanoids: Query<
+        (
+            &mut Transform,
+            &mut Action,
+            &mut CurrentFloor,
+            &mut Direction,
+        ),
+        (With<IsHumanoid>, With<ControledByPlayer>),
+    >,
+    mut wireframe_config: ResMut<Wireframe2dConfig>,
+) {
+    for (mut humanoid_transform, mut action, mut current_floor, mut direction) in &mut humanoids {
+        if keyboard_input.pressed(KeyCode::ArrowLeft)
+            || keyboard_input.pressed(KeyCode::KeyA)
+            || keyboard_input.pressed(KeyCode::ArrowRight)
+            || keyboard_input.pressed(KeyCode::KeyD)
+        {
+            if *action == Action::Walking
+            {
+                // Spielerposition (humanoid_transform) und Zielposition (current_floor)
+                let current_position_x = humanoid_transform.translation[0];
+                let current_position_y = humanoid_transform.translation[1];
+                let mut target_position_x = current_floor.0.clone().unwrap().x_left;
+                let mut target_position_y =
+                    current_floor.0.clone().unwrap().y_left + HGT_HUMANOID / 2.0;
+                if keyboard_input.pressed(KeyCode::ArrowRight)
+                    || keyboard_input.pressed(KeyCode::KeyD)
+                {
+                    *direction = Direction::Right;
+                    target_position_x = current_floor.0.clone().unwrap().x_right;
+                    target_position_y =
+                        current_floor.0.clone().unwrap().y_right + HGT_HUMANOID / 2.0;
+					// Left current floor?
+                    if current_position_x >= target_position_x && current_floor.0.clone().unwrap().id_right_neighbor.is_some() {
+						let new_id: usize = current_floor.0.clone().unwrap().id_right_neighbor.unwrap();
+						let neighbor: Floor = all_floors.0[new_id].clone();
+						*current_floor = CurrentFloor(Some(neighbor));
+					}
+                } else {
+                    *direction = Direction::Left;
+					// Left current floor?
+                    if current_position_x <= target_position_x && current_floor.0.clone().unwrap().id_left_neighbor.is_some() {
+						let new_id: usize = current_floor.0.clone().unwrap().id_left_neighbor.unwrap();
+						let neighbor: Floor = all_floors.0[new_id].clone();
+						*current_floor = CurrentFloor(Some(neighbor));
+					}
+                }
+                // Differenzvektor zwischen Spieler und Ziel
+                let diff_x = target_position_x - current_position_x;
+                let diff_y = target_position_y - current_position_y;
+                // Berechne die Länge (Magnitude) des Vektors
+                let distance = (diff_x * diff_x + diff_y * diff_y).sqrt();
+                // Überprüfe, ob die Distanz größer als ein sehr kleiner Wert ist (um NaN und Nulldivision zu verhindern)
+                if distance > 0.0 {
+                    // Normiere den Differenzvektor (damit die Länge des Vektors 1 beträgt)
+                    let dir_x = diff_x / distance;
+                    let dir_y = diff_y / distance;
+                    // Bewegung in Richtung Ziel multipliziert mit der Geschwindigkeit
+                    let movement_x = dir_x * SPEED_WALKING;
+                    let movement_y = dir_y * SPEED_WALKING;
+                    // Aktualisiere die Spielerposition
+                    humanoid_transform.translation[0] += movement_x;
+                    humanoid_transform.translation[1] += movement_y;
+                }
+            }
+			println!("x: {} y: {} action: {:?} Dir: {:?}", humanoid_transform.translation[0], humanoid_transform.translation[1], *action, *direction);
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+            if *action == Action::Walking {
+                //*action = Action::Falling;
+            }
+            if *action == Action::Digging {
+                // Nach oben graben.
+            }
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+            if *action == Action::Digging {
+                // Nach unten graben.
+            }
+        }
+        if keyboard_input.pressed(KeyCode::ControlLeft)
+            || keyboard_input.pressed(KeyCode::ControlRight)
+        {
+            if *action == Action::Walking {
+                //*action = Action::Digging;
+            } else if *action == Action::Digging {
+                //*action = Action::Walking;
+            }
+        }
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            wireframe_config.global = !wireframe_config.global;
+        }
+    }
+}
